deb452e55d11972935dd560f5787a7a350378348
ProgmanZ <dumphill@gmail.com>
2022-09-06 21:39:35 +0300

    Добавил еще один вариант решения от SkillBox Поис останавливается после нахождения первого совпадающего элемента.

--- Task-02a.py
+++ Task-02a.py
@@ -1,19 +1,22 @@
-# Второй вариант решения задачи
+# Второй вариант решения задачи от SkillBox
+# Правда останавливается при нахождении первого совпадения файла.
 
 import os
 
 
 def search(file_path, file_name):
+    print('Переходим в', file_path)
 
     for element in os.listdir(file_path):
         full_path = os.path.join(file_path, element)
+        print('\tСмотрим', full_path)
 
         if element == file_name:
-            return os.path.join(full_path, file_name)
+            return full_path
 
         if os.path.isdir(full_path):
+            print(full_path.split(os.sep)[-1], 'Это директория')
             result = search(full_path, file_name)
-
             if result:
                 break
 
